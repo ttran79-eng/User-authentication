ead3a0853b73ee83c17a351001c05f221310b5f6
YawnJawn <ttran.96@hotmail.com>
2025-06-04 13:32:53 -0500

    Using mode to toggle log in/create account options

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,13 +1,13 @@
-'use client';
-import Link from 'next/link';
-import { useFormState } from 'react-dom'
+"use client";
+import Link from "next/link";
+import { useFormState } from "react-dom";
 
-import { signup } from '@/actions/auth-actions';
+import { signup } from "@/actions/auth-actions";
 
-export default function AuthForm() {
-  const [formState, formAction] = useFormState(signup,{});
+export default function AuthForm( {mode} ) {
+  const [formState, formAction] = useFormState(signup, {});
   return (
-    <form id="auth-form" action ={formAction}>
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -20,19 +20,28 @@ export default function AuthForm() {
         <input type="password" name="password" id="password" />
       </p>
 
-      {formState.errors && (<ul id="form-errors">
-        {Object.keys(formState.errors).map((error) => <li key={error}>
-            {formState.errors[error]}
-        </li>)}
-      </ul>)}
+      {formState.errors && (
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
 
       <p>
         <button type="submit">
-          Create Account
+          {mode === "login" ? "Login" : "Create Account"}
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {/* If the mode is 'login', show the link to create an account, otherwise show the link to login */}
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an account</Link>
+        )}
+        {/* If the mode is 'signup', show the link to login, otherwise show the link to create an account */}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
